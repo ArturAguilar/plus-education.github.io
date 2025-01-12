6c677e0d9b04008169dd3803065811ee00aecfba
artur <arturaguilar426@gmail.com>
2025-01-12 20:47:57 -0300

    atualização

--- Análise Exploratória de Dados/#01.py
+++ Análise Exploratória de Dados/#01.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-planilha = pd.read_csv('planilhas/enem.csv')
+planilha = pd.read_csv('arquivo.csv') #coloque o arquivo .csv aqui!
 x = planilha.head()
 
 print(x)
