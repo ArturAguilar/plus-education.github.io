5699f3afe3dedc3fddf8b9eaca7745965645fba6
artur <arturaguilar426@gmail.com>
2025-01-12 21:16:44 -0300

    atualização

--- Análise Exploratória de Dados/#01.py
+++ Análise Exploratória de Dados/#01.py
@@ -1,7 +1,17 @@
-import numpy as np
 import pandas as pd
 
-planilha = pd.read_csv('arquivo.csv') #coloque o arquivo .csv aqui!
-x = planilha.head()
+planilha = pd.read_csv('Cuisine_rating.csv') #coloque o arquivo .csv aqui!
+a = planilha.head()
+print(a)
 
-print(x)
+b = planilha.info() #informações sobre a planilha
+print(b)
+
+c = planilha['Cuisines'].value_counts() #contagem da frequência de cada valor na coluna
+print(c)
+
+d = planilha[planilha['Cuisines'] == 'Japanese'] #filtrar a planilha para mostrar apenas as linhas que atendem a condição 
+print(d)
+
+e = planilha[planilha['Cuisines'] == 'Japanese']['Overall Rating'].mean() #calcular a média de uma coluna
+
